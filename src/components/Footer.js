0cf2f013bbc171a0f622cf23b769db520596d664
PriyankaD10 <priyanka.ds10@gmail.com>
2022-11-16 14:39:09 +0530

    committed in main

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+//import { isEmpty, trim } from 'lodash';
+
+class Footer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+            
+        };
+        
+    }
+    
+    
+    render() {
+        return (
+            <div style={{ width: "100%" }}>
+                <footer className="footer">
+                    <span className="text-muted">All rights reserved 2022</span>
+                </footer>
+            </div>
+        )
+    }
+}
+export default Footer;
