69dda412999b0c56ccf196a9ca816977944ae3dd
PriyankaD10 <priyanka.ds10@gmail.com>
2022-11-16 17:45:09 +0530

    fixed redux issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import {Provider} from 'react-redux'
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+import userReducer from './features/user';
 
+const store = configureStore({
+  reducer :  {
+    user:userReducer
+  },
+});
 test('renders learn react link', () => {
-  render(<Provider><App /></Provider>);
+  render(<Provider store={store}><App /></Provider>);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
